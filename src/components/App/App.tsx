b24537fffacaef8cacfd76af3623ccda5e28148c
viktoryka <mikisum321@gmail.com>
2022-02-17 21:33:27 +0100

    feat: deploy gh-pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import { faCircleCheck, faPen, fas } from '@fortawesome/free-solid-svg-icons';
 import React, { FC } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
 import { HomePage } from "../HomePage/HomePage";
@@ -13,19 +13,19 @@ library.add(fas, faGithub, faLinkedin, faCircleCheck, faPen)
 
 export const App: FC = () => {
   return (
-
     <div className={classes.container}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='todo' element={<TodoList />} />
 
-        </Routes>
-      </BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='todo' element={<TodoList />} />
+
+      </Routes>
+
 
       <Footer />
     </div>
+
 
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import { App } from './components/App/App';
 import { store } from './store/index';
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <Provider store={store}>
+
+
+      <App />
+
+
+    </Provider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
